75f976d17be74511fd7191096fd4c3f188e37544
Domingo Ramírez Morales <128168900+Rzmsdo@users.noreply.github.com>
2025-10-19 18:13:40 +0000

    main completado prueba exitosa

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
+from ollama import generate, GenerateResponse
 import uvicorn  
+
 
 app = FastAPI()
  
 @app.get("/")
 async def read_root():
-    return {"¡Que pasa": "Chaval!"}
+    response: GenerateResponse = generate("llama3.2", "¿sabes las diferencias entre una moneda y un billete?")
+    print(response.response)
+    return {"response": response.response}
 
 
 if __name__ == "__main__":
